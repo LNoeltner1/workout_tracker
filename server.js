5e7147d1fcad4b5f0df7af9151097fa0630010e7
Lauren Noeltner <laurennoeltner@gmail.com>
2020-10-23 21:58:40 -0400

    changed workout to workoutDB in mongoose connection in seed.js and server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.static("public"));
 // app.use(logger("dev"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
